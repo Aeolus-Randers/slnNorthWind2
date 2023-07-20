b0479bfc60584f63906bf9fd525ea6baae99b25f
aioria.randers <aioria.randers@gmail.com>
2023-07-20 10:12:35 +0800

    aaa

---
+++ prjNorthWind2/Models/STTClass.cs
@@ -0,0 +1,57 @@
+﻿namespace prjNorthWind2.Models
+{
+    public class STTClass
+    {
+        public int stud_id { get; set; }
+        public int stud_sn { get; set; }
+        public int? stud_entid { get; set; }
+        public string stud_entyear { get; set; }
+        public DateTime? stud_entdate { get; set; }
+        public decimal? stud_entwrite { get; set; }
+        public decimal? stud_entoral { get; set; }
+        public decimal? stud_enttotal { get; set; }
+        public string stud_enttype { get; set; }
+        public string stud_entlevel { get; set; }
+        public bool? stud_prepay { get; set; }
+        public bool? stud_preage { get; set; }
+        public string stud_name { get; set; }
+        public string stud_passname { get; set; }
+        public string stud_engname { get; set; }
+        public bool? stud_sex { get; set; }
+        public string stud_blood { get; set; }
+        public DateTime? stud_birthday { get; set; }
+        public string stud_country { get; set; }
+        public string stud_person_id { get; set; }
+        public string stud_tel_1 { get; set; }
+        public string stud_tel_2 { get; set; }
+        public string stud_tel_3 { get; set; }
+        public string stud_email { get; set; }
+        public string stud_addr1_post { get; set; }
+        public string stud_addr1_city { get; set; }
+        public string stud_addr1_district { get; set; }
+        public string stud_addr1_road { get; set; }
+        public string stud_addr1_addr { get; set; }
+        public string stud_addr2_post { get; set; }
+        public string stud_addr2_city { get; set; }
+        public string stud_addr2_district { get; set; }
+        public string stud_addr2_road { get; set; }
+        public string stud_addr2_addr { get; set; }
+        public string stud_kdschool { get; set; }
+        public string stud_kdclass { get; set; }
+        public string stud_kdteacher { get; set; }
+        public string stud_primschool { get; set; }
+        public string stud_primgrade { get; set; }
+        public string stud_primclass { get; set; }
+        public string stud_primteacher { get; set; }
+        public string stud_lereason { get; set; }
+        public string stud_ledetail { get; set; }
+        public DateTime? stud_ledate { get; set; }
+        public string stud_leschool { get; set; }
+        public string stud_meat { get; set; }
+        public string stud_memo { get; set; }
+        public string stud_gdkind { get; set; }
+        public DateTime? stud_gddate { get; set; }
+        public string stud_gdword { get; set; }
+        public string stud_gdnum { get; set; }
+    }
+}
